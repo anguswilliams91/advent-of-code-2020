80d81289990759827e688df656ec398601eabbae
Angus Williams <anguswilliams91@gmail.com>
2020-12-26 10:50:43 +0000

    tidy 7

--- 07/main.py
+++ 07/main.py
@@ -1,44 +1,15 @@
 """7. Count how many bags another bag can go be stored in."""
 from collections import defaultdict
-from typing import Dict, List, Tuple
+from typing import DefaultDict, Dict, List, Tuple
 import re
 
 OUTER_BAG_REGEX = re.compile(r"([a-z]+ [a-z]+) bags contain ")
 INNER_BAG_REGEX = re.compile(r"(\d) ([a-z]+ [a-z]+) bag[s]{0,1}[.,]{0,1}")
 
 
-class BagGraph:
-    """Directed graph with bag colours as nodes."""
-
-    def __init__(self, rules: List[Dict[str, List[Tuple[int, str]]]]):
-        self.bag_graph = defaultdict(set)
-
-        for parent_to_children in rules:
-            for parent, children in parent_to_children.items():
-                for (_, child) in children:
-                    self.insert(child, parent)
-
-    def insert(self, x: str, y: str):
-        self.bag_graph[x].add(y)
-
-    def breadth_first_search(self, first: str) -> int:
-        # standard bfs using a queue
-        visited = defaultdict(bool)
-
-        queue = [first]
-        visited[first] = True
-
-        while queue:
-            current = queue.pop(0)
-            for other in self.bag_graph[current]:
-                if not visited[other]:
-                    visited[other] = True
-                    queue.append(other)
-
-        return sum(visited.values())
-
-
-def extract_rules_from_string(string_rules: List[str]) -> BagGraph:
+def extract_rules_from_string(
+    string_rules: List[str]
+) -> List[Dict[str, List[Tuple[int, str]]]]:
     # extract the rules from the string representation
     rules = []
     for rule in string_rules:
@@ -54,8 +25,27 @@ def extract_rules_from_string(string_rules: List[str]) -> BagGraph:
 
 def count_possible_outer_bags(string_rules: List[str]) -> int:
     # part 1 - count number of possible outer bag colours
-    bag_graph = BagGraph(extract_rules_from_string(string_rules))
-    return bag_graph.breadth_first_search("shiny gold") - 1
+    rules = extract_rules_from_string(string_rules)
+
+    bag_graph = defaultdict(set)
+    for parent_to_children in rules:
+        for parent, children in parent_to_children.items():
+            for (_, child) in children:
+                bag_graph[child].add(parent)
+
+    # breadth first search
+    visited = defaultdict(bool)
+    queue = ["shiny gold"]
+    visited["shiny gold"] = True
+
+    while queue:
+        current = queue.pop(0)
+        for other in bag_graph[current]:
+            if not visited[other]:
+                visited[other] = True
+                queue.append(other)
+
+    return sum(visited.values()) - 1
 
 
 def count_inner_bags(rules: Dict[str, List[Tuple[int, str]]], colour):
