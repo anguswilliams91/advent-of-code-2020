1c71f3cfa66d75445b3432ee1087f68ff3a618c0
Angus Williams <anguswilliams91@gmail.com>
2020-12-27 14:59:18 +0000

    remove unnecessary bool from return

--- 22/main.py
+++ 22/main.py
@@ -41,16 +41,14 @@ def play_recursive_combat(
     deck_1: List[int],
     deck_2: List[int],
     memory: Set[Tuple[List[int], List[int]]] = set(),
-) -> (bool, List[int], List[int]):
+) -> (List[int], List[int]):
     # play the recursive version of the game
 
     is_a_winner = False
-    won_by_short_circuit = False
     while not is_a_winner:
 
         if (tuple(deck_1), tuple(deck_2)) in memory:
             is_a_winner = True
-            won_by_short_circuit = True
 
         else:
             memory.add((tuple(deck_1), tuple(deck_2)))
@@ -59,11 +57,11 @@ def play_recursive_combat(
             n_2 = deck_2.pop(0)
 
             if len(deck_1) >= n_1 and len(deck_2) >= n_2:
-                sub_won_by_short_circuit, _, sub_deck_2 = play_recursive_combat(
+                sub_deck_1, sub_deck_2 = play_recursive_combat(
                     copy(deck_1[:n_1]), copy(deck_2[:n_2]), memory=set()
                 )
 
-                if sub_won_by_short_circuit or not sub_deck_2:
+                if (sub_deck_1 and sub_deck_2) or not sub_deck_2:
                     deck_1.extend([n_1, n_2])
                 else:
                     deck_2.extend([n_2, n_1])
@@ -77,13 +75,13 @@ def play_recursive_combat(
                 if not deck_1 or not deck_2:
                     is_a_winner = True
 
-    return won_by_short_circuit, deck_1, deck_2
+    return deck_1, deck_2
 
 
 def part_two(deck_1: List[int], deck_2: List[int]) -> int:
     # play the crab at recursive combat
-    won_by_short_circuit, deck_1, deck_2 = play_recursive_combat(deck_1, deck_2)
-    winners_deck = deck_1 if (won_by_short_circuit or not deck_2) else deck_2
+    deck_1, deck_2 = play_recursive_combat(deck_1, deck_2)
+    winners_deck = deck_1 if (deck_1 and deck_2) or not deck_2 else deck_2
     n = len(winners_deck)
     return sum(i * j for i, j in zip(range(1, n + 1), winners_deck[::-1]))
 
