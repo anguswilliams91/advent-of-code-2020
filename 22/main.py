996e547d8225d5de34e04258b3e97fb7725a02b0
Angus Williams <anguswilliams91@gmail.com>
2020-12-27 14:48:53 +0000

    day 22

---
+++ 22/main.py
@@ -0,0 +1,103 @@
+"""22. Crab cards."""
+from copy import copy
+from typing import List, Optional, Set, Tuple
+
+
+def parse_input(input_string: str) -> (List[int], List[int]):
+    # extract the starting decks from the input
+    deck_1, deck_2 = input_string.split("\n\n")
+    deck_1 = [int(i) for i in deck_1.splitlines()[1:]]
+    deck_2 = [int(i) for i in deck_2.splitlines()[1:]]
+    return deck_1, deck_2
+
+
+def play_combat(deck_1: List[int], deck_2: List[int]) -> (List[int], List[int]):
+    # play the game until there's a winner
+    is_a_winner = False
+    while not is_a_winner:
+        n_1 = deck_1.pop(0)
+        n_2 = deck_2.pop(0)
+
+        if n_1 > n_2:
+            deck_1.extend([n_1, n_2])
+        else:
+            deck_2.extend([n_2, n_1])
+
+        if not deck_1 or not deck_2:
+            is_a_winner = True
+
+    return deck_1, deck_2
+
+
+def part_one(deck_1: List[int], deck_2: List[int]) -> int:
+    # play the game and then return the winner's score
+    n = len(deck_1) + len(deck_2)
+    deck_1, deck_2 = play_combat(deck_1, deck_2)
+
+    winners_deck = deck_1 or deck_2
+
+    return sum(i * j for i, j in zip(range(1, n + 1), winners_deck[::-1]))
+
+
+def play_recursive_combat(
+    deck_1: List[int],
+    deck_2: List[int],
+    memory: Set[Tuple[List[int], List[int]]] = set(),
+) -> (bool, List[int], List[int]):
+    # play the recursive version of the game
+
+    is_a_winner = False
+    won_by_short_circuit = False
+    while not is_a_winner:
+
+        if (tuple(deck_1), tuple(deck_2)) in memory:
+            is_a_winner = True
+            won_by_short_circuit = True
+
+        else:
+            memory.add((tuple(deck_1), tuple(deck_2)))
+
+            n_1 = deck_1.pop(0)
+            n_2 = deck_2.pop(0)
+
+            if len(deck_1) >= n_1 and len(deck_2) >= n_2:
+                sub_won_by_short_circuit, _, sub_deck_2 = play_recursive_combat(
+                    copy(deck_1[:n_1]), copy(deck_2[:n_2]), memory=set()
+                )
+
+                if sub_won_by_short_circuit or not sub_deck_2:
+                    deck_1.extend([n_1, n_2])
+                else:
+                    deck_2.extend([n_2, n_1])
+
+            else:
+                if n_1 > n_2:
+                    deck_1.extend([n_1, n_2])
+                else:
+                    deck_2.extend([n_2, n_1])
+
+                if not deck_1 or not deck_2:
+                    is_a_winner = True
+
+    return won_by_short_circuit, deck_1, deck_2
+
+
+def part_two(deck_1: List[int], deck_2: List[int]) -> int:
+    # play the crab at recursive combat
+    won_by_short_circuit, deck_1, deck_2 = play_recursive_combat(deck_1, deck_2)
+
+    winners_deck = deck_1 if (won_by_short_circuit or not deck_2) else deck_2
+    n = len(winners_deck)
+
+    return sum(i * j for i, j in zip(range(1, n + 1), winners_deck[::-1]))
+
+
+if __name__ == "__main__":
+
+    with open("input.txt", "r") as f:
+        input_string = f.read()
+
+    deck_1, deck_2 = parse_input(input_string)
+
+    print(part_one(copy(deck_1), copy(deck_2)))
+    print(part_two(copy(deck_1), copy(deck_2)))
