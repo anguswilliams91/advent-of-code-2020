b7ff8c7338b647358440386630bfde4be8cc3d9d
Angus Williams <anguswilliams91@gmail.com>
2020-12-28 13:14:12 +0000

    day 25

---
+++ 25/main.py
@@ -0,0 +1,40 @@
+"""25. RFID codes."""
+C = 20201227
+
+
+def find_loop_size(starting_value: int, public_key: int) -> int:
+    # find the loop size by brute force
+    key = starting_value
+    loop_size = 0
+    while key != public_key:
+        key = (key * 7) % C
+        loop_size += 1
+
+    return loop_size
+
+
+def find_encryption_key(door_key: int, card_key: int) -> int:
+    # find the encryption key implied by the door and card keys
+    door_loop_size = find_loop_size(1, door_key)
+    card_loop_size = find_loop_size(1, card_key)
+
+    if door_loop_size < card_loop_size:
+        subject_number = card_key
+        loops = door_loop_size
+    else:
+        subject_number = door_key
+        loops = card_loop_size
+
+    encryption_key = 1
+    for _ in range(loops):
+        encryption_key = (encryption_key * subject_number) % C
+
+    return encryption_key
+
+
+if __name__ == "__main__":
+
+    card_key = 2084668
+    door_key = 3704642
+
+    print(find_encryption_key(door_key, card_key))
