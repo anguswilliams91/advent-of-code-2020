87283eef19dd34052fdb06111cef8bc2b3827899
Angus Williams <anguswilliams91@gmail.com>
2020-12-29 22:29:19 +0000

    add utility for running and timing solutions

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # aoc-2020
-Advent of code 2020
+
+My solutions to advent of code 2020, in python.
+
+To reproduce the results, create a python environment (version 3.8) and install the requirements:
+
+```
+pip install -r requirements.txt
+```
+
+Run a specific day with e.g.
+
+```
+make run day=1
+```
+
+and the two results will be printed to the screen, along with the runtime.
+To run all the days at once, do
+
+```
+make run day=all
+```
+
+
+
+
---
+++ advent.py
@@ -0,0 +1,39 @@
+"""Utility to run solutions."""
+from argparse import ArgumentParser
+import os
+import time
+import pathlib
+from typing import Union
+
+BASE_PATH = pathlib.Path(__file__).parent.absolute()
+
+
+def run_day(day: Union[int, str]):
+    # run the given day
+    if day not in range(1, 26):
+        raise ValueError("day must be between 1 and 25!")
+
+    print(f"Running day {day}.")
+    start = time.time()
+    solution_dir = BASE_PATH / str(day).zfill(2)
+    os.chdir(solution_dir)
+    os.system("python main.py")
+    end = time.time()
+    print(f"Took {end - start: .2f} seconds.\n")
+
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser()
+    parser.add_argument(dest="day", help="Run the solution to this day of AOC.")
+    args = parser.parse_args()
+
+    if args.day != "all":
+        run_day(args.day)
+
+    else:
+        overall_start = time.time()
+        for day in range(1, 26):
+            run_day(day)
+        overall_end = time.time()
+        print(f"Total time taken: {overall_end - overall_start: .1f} seconds.")
